c5b8e5971b1b6c991dd6e871f5ed329453c659c3
HarshuYs124 <132802431+HarshuYs124@users.noreply.github.com>
2026-03-13 23:06:40 +0530

    update project files

--- Class/Practice/12-Mar-tasks/Task2.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver import Chrome,ChromeOptions
-o=ChromeOptions()
-o.add_experimental_option("detach",True)
-driver=Chrome(options=o)
-driver.get("https://www.wikipedia.com")
-print(driver.current_url)
