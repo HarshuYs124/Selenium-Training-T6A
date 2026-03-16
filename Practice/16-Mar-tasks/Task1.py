34b614c2efe3649b0d46fee4cc20a6f77ce03207
HarshuYs124 <132802431+HarshuYs124@users.noreply.github.com>
2026-03-16 23:08:11 +0530

    update

---
+++ Practice/16-Mar-tasks/Task1.py
@@ -0,0 +1,21 @@
+from time import sleep
+from selenium.webdriver import Chrome,ChromeOptions
+from selenium.webdriver.common.by import By
+o=ChromeOptions()
+# o.add_argument('--headless')
+o.add_experimental_option("detach",True)
+driver=Chrome(options=o)
+
+driver.get("https://www.amazon.in/")
+driver.maximize_window()
+sleep(2)
+driver.find_element(By.NAME,'field-keywords').send_keys("mobiles")
+sleep(2)
+driver.find_element(By.ID,'nav-search-submit-button').click()
+sleep(2)
+driver.find_element(By.XPATH,"(//h2/span)[1]")
+
+# price=driver.find_element(By.XPATH,"(//span[@class='a-price-whole'])[3]")
+price=driver.find_element(By.XPATH,"//span[text()='iQOO Z10x 5G (Ultramarine, 8GB RAM, 256GB Storage) | 6500 mAh Large Capacity Battery | Dimensity 7300 Processor | Military-Grade Durability']/../../../..//span[@class='a-price-whole']").text
+print("price of phone is: ",price)
+
---
+++ Practice/16-Mar-tasks/Task2.py
@@ -0,0 +1,17 @@
+from time import sleep
+from selenium.webdriver import Chrome,ChromeOptions
+from selenium.webdriver.common.by import By
+o=ChromeOptions()
+# o.add_argument('--headless')
+o.add_experimental_option("detach",True)
+driver=Chrome(options=o)
+
+driver.get("https://www.flipkart.com/")
+driver.maximize_window()
+sleep(2)
+driver.find_element(By.NAME,'q').send_keys("watches for women")
+sleep(2)
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
+sleep(2)
+price = driver.find_element(By.XPATH, "//a[text()='Big Dial Analog Watch  - For Women 106408499']/../../../..//div[contains(text(),'₹')]").text
+print(price)
