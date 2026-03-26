00a9f3dcd42b15dd8305b388e2600a353f5729a5
Harshita Yadav <132802431+HarshuYs124@users.noreply.github.com>
2026-03-26 10:57:48 +0530

    Update 24-Mar.py

--- Class/24-Mar.py
+++ Class/24-Mar.py
@@ -2,15 +2,7 @@
 # make three webpages with second's address in first
 from time import sleep
 from selenium.webdriver import Chrome,ChromeOptions
-from selenium.webdriver.common import actions
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
-from websocket import send
 
 o=ChromeOptions()
 o.add_experimental_option("detach",True)
